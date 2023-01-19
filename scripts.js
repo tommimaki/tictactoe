34d493fc3674d82211ece96a72cdaca72bf37264
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-01-19 20:56:22 +0200

    init

---
+++ scripts.js
@@ -0,0 +1,25 @@
+//gameboard object, module pattern
+const gameBoard = (() =>{
+
+    const grid = [1,2,3,
+                   4,5,6,
+                    7,8,9]
+    return{
+        //jotain
+    }
+})();
+
+
+
+// player object
+
+const Player = (move, score) => {
+    
+        const move = x =>{
+            //set X / O
+        }
+    };
+
+    const Controller = (score) => {
+        const score = 0;
+    }
